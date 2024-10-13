48493fe24e284229e986b9f88c981ce5cd680543
pablomarquezb78 <pablomarquezbenitez78@gmail.com>
2024-10-13 11:53:17 +0200

    Pequeña revisión + mejora comentarios

--- ThreesProject/ai.py
+++ ThreesProject/ai.py
@@ -6,102 +6,137 @@ reglasProducción = [const.Movimiento.ABAJO,const.Movimiento.ARRIBA,const.Movimi
 
 import board as bd
 
-"""
-Creamos un nodo con la estructura de Node pero con los atributos personalizados:
-    board: Tablero con la situación actual de juego
-    movimiento: Movimiento que condujo del padre a ese estado de tablero
-    valor_heuristico: Por defecto cero para cuando no se utiliza, valor obtenido del cálculo de una función heurística sobre el nodo
-Esta clase contiene además un atributo valor_F, este valor será la suma de la profundidad a la que se encuentra nodo en el árbol + el valor heurístico proporcionado
-"""
 class MiNodo(Node):
+    """
+    Creamos un nodo con la estructura de Node pero con atributos personalizados:
+
+    :param board Tablero con la situación actual de juego
+    :param movimiento: Movimiento que condujo del padre a ese estado de tablero
+    :param valor_heuristico: Por defecto cero para cuando no se utiliza, valor obtenido del cálculo de una función heurística sobre el nodo
+    :param valor_F: este valor será la suma de la profundidad a la que se encuentra nodo en el árbol (coste g(n)) + el valor heurístico proporcionado (h(n))
+    """
     def __init__(self, name, padre, board, movimiento, valor_heuristico=0):
         super().__init__(name, padre)
         self.board = board
-        self.valor_F = self.depth + valor_heuristico # El coste de la función para lo que llevo hasta ahora corresponde con la profundidad + valor heurístico calculado
-        self.movimiento = movimiento # Guardamos que movimiento desde el padre ha llevado a este estado
+        self.valor_F = self.depth + valor_heuristico
+        self.movimiento = movimiento
 
-# En este problema carecemos de estado objetivo, se juega hasta que la lista de abiertos esté vacia y luego nos quedamos con el mejor resultado
 class Ai:
 
+    """
+    Este constructor inicializa el algoritmo de búsqueda dado un tablero,
+    inicializa la lista de abiertos y cerrados como lístas vacías, guarda
+    el estado inicial del tablero, la raíz del arbol con el valor hash del tablero como
+    nombre, con el board, sin padre ni movimiento y con el valor de la función heurística
+    en el estado inicial. También guarda siempre el nodo con mejor puntuación y la
+    función heurística en caso de que proceda.
+
+    :param board: instancia inicial de la clase tablero en una nueva partida de Threes!
+    :param funcion_heuristica: función heurística que se usará en el algoritmo de búsqueda
+    por defecto es nula.
+    """
     def __init__(self, board, funcion_heuristica=lambda x: 0):
-        # Lista abiertos
         self.abiertos = []
-        # Lista cerrados
         self.cerrados = []
-        # Estado inicial
         self.estadoInicial = board
-        # Guardamos la raíz árbol de búsqueda
         self.raiz = MiNodo(board.__hash__(), board=board, padre=None, movimiento=None, valor_heuristico=funcion_heuristica(self.estadoInicial))
-        # Guardamos el nodo ganador, inicialmente raíz
         self.nodo_ganador = self.raiz
-        # Guardamos la función heurística que se utilizará si se usa un algoritmo con ella
         self.funcion_heuristica = funcion_heuristica
 
-        self.cont = 0
-
-    # Se usa esta función para obtener el camino del nodo objetivo a la raíz
-    def encontrar_path(self, nodo):
+    """
+    Recorre el árbol desde el nodo ganador hasta la raíz guardando los tableros en una lista
+    
+    :param nodoGanador: nodo con más puntuación encontrado con un algoritmo de búsqueda determinado
+    :return: Lista de tableros ordenada desde la raíz hasta el nodoGanador
+    """
+    def encontrar_path(self, nodo_ganador):
         path = []
-        while nodo is not None:
-            path.append(nodo.board)  # Guardamos el tablero en el camino
-            nodo = nodo.parent  # Movemos al padre
-        return path[::-1]  # Invertimos el camino
+        while nodo_ganador is not None:
+            path.append(nodo_ganador.board)
+            nodo_ganador = nodo_ganador.parent
+        return path[::-1]
 
-    # Dibuja el arbol y lo exporta en formato .dot
+    """
+    Muestra por consola el árbol de búsqueda obtenido tras desarrollar un algoritmo de búsqueda
+    también importa este árbol en formato .dot
+    """
     def mostrar_arbol(self):
         for pre, fill, node in RenderTree(self.raiz):
             print(f"{pre}{node.name} (Puntuación: {node.board.calcularPuntuacion()})")
-        # Exportamos el árbol a un formato gráfico (graphviz)
         DotExporter(self.raiz).to_dotfile("arbol_busqueda.dot")
 
-    # Añade a abiertos los nodos resultantes de la expansión del nodo "nodo_padre"
+    """
+    Añade a abiertos los nodos resultantes de la expansión del nodo "nodo_padre" aplicando
+    las  diferentes reglas de producción del juego Threes!. Se guardan los tableros a expandir como 
+    una copia independiente del nodo padre para evitar que la regla de producción altere al 
+    nodo padre.
+    
+    Cada regla de producción se intenta aplicar a la copia del tablero padre y se comprueba que se 
+    obtenga un nuevo tablero y este no haya sido explorado antes (no se encuentra ni en abiertos ni
+    en cerrados) si se cumple esta condición, se calcula su heurístico, se crea el nodo hijo cuyo nombre 
+    es el hash del nuevo tablero y guarda el nuevo tablero, un puntero al nodo padre, el movimiento que
+    se acaba de aplicar y el valor heurístico calculado.
+    
+    Para cada nuevo nodo se comprueba que sea el de mejor puntuación en tal caso se guarda.
+    
+    :param nodo_padre:  nodo que queremos expandir.
+    """
     def calcularAbiertos(self, nodo_padre):
         for i in range (len(reglasProducción)):
-            # Obtenemos el tablero del nodo padre
             padre = nodo_padre.board
-            # Creamos un tablero nuevo para evitar usar la misma referencia y alterar el nodo padre
             nuevoTablero = bd.Board.__copy__(padre)
 
-            # Copiamos el tablero del padre al nuevo tablero
-
-            #Movemos el nuevo tablero según la regla de producción correspondiente
             nuevoTablero.moverTablero(reglasProducción[i])
 
-            #Si la regla ha sido aplicable y obtenemos tableros que no estén en cerrados ni abiertos (nodo nuevo)
-            if nuevoTablero is not None and self.esNuevo(nuevoTablero):
-                    # Calculamos el heurístico
+            if nuevoTablero and self.esNuevo(nuevoTablero):
                     resultado_heuristico = self.funcion_heuristica(nuevoTablero);
                 
-                    # Añadimos información al arbol creando un nodoHijo y poniendo el puntero al nodo padre
                     nodoHijo = MiNodo(nuevoTablero.__hash__(),board = nuevoTablero, padre=nodo_padre, movimiento=reglasProducción[i], valor_heuristico=resultado_heuristico)
 
-                    # Mantenemos actualizado el estado con mejor puntuación
                     if bd.Board.calcularPuntuacion(self.nodo_ganador.board) < bd.Board.calcularPuntuacion(nuevoTablero):
                         self.nodo_ganador = nodoHijo
 
-                    #Añadimos el nodo a abiertos
                     self.abiertos.append(nodoHijo)
 
-    # Comprueba usando equals si el nuevo tablero se encuentra en cerrados
+    """ 
+    Comprueba usando equals si un tablero se encuentra en cerrados
+    
+    :param tablero: tablero a buscar en la lista de cerrados
+    :return: True si está en cerrados False en caso contrario
+    """
     def estaEnCerrados(self, tablero):
         for cerrado in self.cerrados:
             if tablero.__eq__(cerrado.board):
                 return True
         return False
 
-    # Comprueba usando equals si el nuevo tablero se encuentra en abiertos
+    """ 
+    Comprueba usando equals si un tablero se encuentra en abiertos
+
+    :param tablero: tablero a buscar en la lista de abiertos
+    :return: True si está en abiertos False en caso contrario
+    """
     def estaEnAbiertos(self, tablero):
         for abierto in self.abiertos:
             if tablero.__eq__(abierto.board):
                 return True
         return False
 
-    # Nos dice si un tablero es nuevo en el árbol de exploración
+    """
+    Nos dice si un tablero es nuevo en el árbol de exploración
+    comprobando que no esté en cerrados ni abiertos
+    :param tablero: tablero a comprbar
+    _return: True si es nuevo en la búsueda False en caso contrario
+    """
     def esNuevo(self,tablero):
         return not self.estaEnCerrados(tablero) and not self.estaEnAbiertos(tablero)
 
+    """
+    Muestra los resultados tras la aplicación de un algoritmo de búsqueda
+    Mostrando por pantalla el estado con mejor puntución y el camino desde la raíz
+    a ese estado.
+    """
     def mostrarResultado(self):
-        # Mostramos resultados obtenidos
         print("\nEstado ganador:\n", self.nodo_ganador.board.board)
         print("\nPath ganador:")
         path = self.encontrar_path(self.nodo_ganador)
@@ -110,13 +145,13 @@ class Ai:
             print("\n")
     
     """
-    Devuelve el índice del nodo con valor_F mejor (según se le indique)
-        porMenorValorF: 
-            True si busca el nodo con menor valor_F,
-            False si busca el nodo con mayor valor_F
-        porAntiguedad: 
-            True si siempre se queda con el primer nodo que encuentra con mejor valor_F (el más antiguo)
-            False si siempre se queda con el último nodo que encuentra con mejor valor_F (el más nuevo)
+    :param porMenorValorF: 
+        True si busca el nodo con menor valor_F,
+        False si busca el nodo con mayor valor_F
+    :param porAntiguedad: 
+        True si siempre se queda con el primer nodo que encuentra con mejor valor_F (el más antiguo)
+        False si siempre se queda con el último nodo que encuentra con mejor valor_F (el más nuevo)
+    :return: indice del nodo con mejor valor_F según criterio dado
     """
     def obtenerIndiceMejorValorF(self, porMenorValorF=True, porAntiguedad=True):
         mejor_valor_F = self.abiertos[0].valor_F;
@@ -136,44 +171,48 @@ class Ai:
             
         return indice_mejor_nodo;    
 
-    # Búsqueda en amplitud, tiene el parámetro opcional max_steps para regular el número de expansiones permitidos en la exploracion, si no acotamos, tardará mucho...
+    """
+    Búsqueda en amplitud, calcula los abiertos de la raiz y va iterativamente
+    sacando nodos de abiertos (antiguos primeros), metiendolos en cerrados y
+    calculando los abiertos del nuevo nodo hasta que se de la condición de parada
+    que puede ser que se alcance al máximo de expansiones permitidas o que no queden
+    más nodos por abrir (se acaba el propio juego). Tras finalizar siempre muestra resultados
+    
+    :param max_steps: parámetro opcional para regular el número de expansiones permitidos en la exploracion
+    por defecto tiene valor 100
+    """
     def BFS(self,max_steps=100):
-        # Calculamos nodos abiertos en la raíz
         self.calcularAbiertos(self.raiz)
-        # La lista de nodos cerrados se inicializa a vacia en el propio constructor
         step = 1
         while step<=max_steps:
-            # Sacamos el nodo n de abiertos, al sacar el primero estamos sacando el de mayor antigüedad (Amplitud)
             n = self.abiertos.pop(0)
-            # Añadimos nodo n a Cerrados
             self.cerrados.append(n)
-            # Expandimos n obteniendo nuevos nodos en abiertos
             self.calcularAbiertos(n)
 
-            #Avanzamos un paso de expansion
             step += 1
 
-        # Mostramos resultados obtenidos
         self.mostrarResultado()
 
-    # Búsqueda en profundidad, tiene el parámetro opcional max_steps para regular el número de expansiones permitidos en la exploracion, si no acotamos, tardará mucho...
+    """
+    Búsqueda en profundidad, calcula los abiertos de la raiz y va iterativamente
+    sacando nodos de abiertos (nuevos primeros), metiendolos en cerrados y
+    calculando los abiertos del nuevo nodo hasta que se de la condición de parada
+    que puede ser que se alcance al máximo de expansiones permitidas o  que no queden
+    más nodos por abrir (se acaba el propio juego). Tras finalizar siempre muestra resultados
+
+    :param max_steps: parámetro opcional para regular el número de expansiones permitidos en la exploracion
+    por defecto tiene valor 100
+    """
     def DFS(self,max_steps=100):
-        # Calculamos nodos abiertos en la raíz
         self.calcularAbiertos(self.raiz)
-        # La lista de nodos cerrados se inicializa a vacia en el propio constructor
         step = 1
         while step <= max_steps:
-            # Sacamos el nodo n de abiertos, al sacar el último estamos sacando el de menor antigüedad
             n = self.abiertos.pop()
-            # Añadimos nodo n a Cerrados
             self.cerrados.append(n)
-            # Expandimos n obteniendo nuevos nodos en abiertos
             self.calcularAbiertos(n)
 
-            #Avanzamos un paso de expansion
             step += 1
 
-        # Mostramos resultados obtenidos
         self.mostrarResultado()
         
     # Algoritmo A*
